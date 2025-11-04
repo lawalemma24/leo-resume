c652744b2164906f29d2363b52ba331a3a834784
lawalemma24 <lawalemma24@gmail.com>
2025-11-04 22:21:19 +0100

    final

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -50,7 +50,7 @@ const HeroSection = () => {
               href="/"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-5">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2 mb-4" >
                 Download CV
               </span>
             </Link>
@@ -62,7 +62,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]  relative">
             <Image
               src="/images/profile-pics.jpg"
               alt="hero image"
