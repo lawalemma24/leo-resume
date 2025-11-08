40a1b8fa317bed239a27afbf207653c2b874de7d
lawalemma24 <lawalemma24@gmail.com>
2025-11-08 06:53:06 +0100

    fixes

--- src/app/components/AchievementSection.jsx
+++ src/app/components/AchievementSection.jsx
@@ -12,21 +12,21 @@ const AnimatedNumbers = dynamic(
 const achievementsList = [
   {
     metric: "Projects",
-    value: "100",
+    value: "20",
     postfix: "+",
   },
   {
     prefix: "~",
     metric: "Users",
-    value: "100,000",
+    value: "10",
   },
   {
     metric: "Awards",
-    value: "7",
+    value: "2",
   },
   {
     metric: "Years",
-    value: "5",
+    value: "2",
   },
 ];
 
@@ -50,7 +50,7 @@ const AchievementSection = () => {
                   configs={(_, index) => {
                     return {
                       mass: 1,
-                      friction: 100,
+                      friction: 40,
                       tensions: 140 * (index + 1),
                     };
                   }}
