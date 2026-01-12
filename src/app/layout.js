2c06af6f16683ad4ea35ab13be4bf739fac3b457
lawalemma24 <lawalemma24@gmail.com>
2026-01-12 01:45:22 +0100

    jay done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,16 @@
-// import { Geist, Geist_Mono } from "next/font/google";
+// src/app/layout.js
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata = {
   title: "LEO Portfolio",
   description: "Portfolio Website",
-
-  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         {children}
         <Toaster />
       </body>
