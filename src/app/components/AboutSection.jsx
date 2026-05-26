b6835a221b10fc51912df42287c246f4e0215003
lawalemma24 <lawalemma24@gmail.com>
2026-05-26 14:54:14 +0100

    skills fixes

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -70,7 +70,7 @@ const AboutSection = () => {
           <p className="text-base lg:text-lg">
             I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, C++, Express, PostgreSQL,
+            working with ASP.Net, C#, JavaScript, React, Redux, Node.js, Express, PostgreSQL,
             HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill sets. I am a team player and
             I am so excited to work with others to create amazing Software applications.
